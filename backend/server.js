99fdaa2e9a3c5b1178c63b7ec6e190abc3f1876d
Saurabh <risabht043@gmail.com>
2024-12-14 16:24:34 +0530

    done signup, login, logout, getMe

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import authRoutes from './routes/auth.routes.js'
 import dotenv from 'dotenv'
 import connectMongoDb from "./db/connect.js";
+import cookieParser from "cookie-parser";
 
 
 const app = express();
@@ -11,13 +12,11 @@ dotenv.config();
 
 const Port = process.env.PORT || 5000;
 
-
+app.use(express.json()); //to parse req body
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes)
-
-app.get("/", (req, res) => {
-    res.send("Server is ready");
-})
 
 app.listen(Port, () => {
    console.log(`server is running on this: ${Port}`);
