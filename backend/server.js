39b3bcffd5cff69543646f46d4bcde0b8b369540
Saurabh <risabht043@gmail.com>
2025-02-09 23:08:31 +0530

    backend done

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import authRoutes from './routes/auth.routes.js'
 import userRoutes from './routes/user.routes.js'
 import postRoutes from './routes/post.routes.js'
+import notificationRoutes from './routes/notification.routes.js'
 import dotenv from 'dotenv'
 import connectMongoDb from "./db/connect.js";
 import cookieParser from "cookie-parser";
@@ -27,6 +28,7 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes)
 app.use("/api/users", userRoutes)
 app.use("/api/posts", postRoutes)
+app.use("/api/notifications", notificationRoutes)
 
 app.listen(Port, () => {
    console.log(`server is running on this: ${Port}`);
